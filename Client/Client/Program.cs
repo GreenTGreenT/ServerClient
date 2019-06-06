a31d8879ff8a5331e70418278078ec925b49cdc7
GreenTGreenT <treorey@gmail.com>
2019-06-06 18:03:47 +0700

    Server and client can communicate with each other through protocol but protocol not complete

---
+++ Client/Client/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+   
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    int port = 11000;
+        //    string IpAddress = "127.0.0.1";
+        //    Socket ClientSocket = new Socket(AddressFamily
+        //        .InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //    IPEndPoint ep = new IPEndPoint(IPAddress.Parse(IpAddress), port);
+        //    ClientSocket.Connect(ep);
+        //    Console.WriteLine("You are Connecting!");
+        //    while(true)
+        //    {
+        //        string messageFromClient = null;
+        //        Console.WriteLine("Enter the Message");
+        //        messageFromClient = Console.ReadLine();
+        //        ClientSocket.Send(System.Text.Encoding.ASCII
+        //            .GetBytes(messageFromClient), 0, messageFromClient.Length, SocketFlags.None);
+        //        byte[] MsgFromServer = new byte[1024];
+        //        int size = ClientSocket.Receive(MsgFromServer);
+        //        Console.WriteLine("Sever " + System.Text.Encoding.ASCII.GetString(MsgFromServer, 0, size));
+        //    }
+        //}
+
+         
+        static void Main(string[] args)
+        {
+            List<byte> bytelist = new List<byte>();
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 5000;
+            TcpClient client = new TcpClient();
+            client.Connect(ip, port);
+            Console.WriteLine("client connected!!");
+            NetworkStream ns = client.GetStream();
+            Thread thread = new Thread(o => ReceiveData((TcpClient)o));
+
+            thread.Start(client);
+
+            string s;
+            while (!string.IsNullOrEmpty((s = Console.ReadLine())))
+            {
+                //byte[] buffer = Encoding.ASCII.GetBytes(s);
+                //ns.Write(buffer, 0, buffer.Length); 
+
+                int id = int.Parse(s);
+
+
+
+                List<byte> sendByte = new List<byte>();
+                sendByte.Add(0xff);
+                sendByte.Add(0x04);
+                sendByte.Add((byte)id);
+
+
+                string status = "Datetime:2019-06-06 23:59:59|SensorId:1|Status:1";
+
+                byte[] data = Encoding.ASCII.GetBytes(status);
+
+                for(int i=0;i<data.Length;i++)
+                {
+                    sendByte.Add(data[i]);
+                }
+
+                ns.Write(sendByte.ToArray(), 0, sendByte.Count);
+
+            }
+            
+            client.Client.Shutdown(SocketShutdown.Send);
+            thread.Join();
+            ns.Close();
+            client.Close();
+            Console.WriteLine("disconnect from server!!");
+            Console.ReadKey();
+        }
+
+        static void ReceiveData(TcpClient client)
+        {
+            NetworkStream ns = client.GetStream();
+            byte[] receivedBytes = new byte[1024];
+            int byte_count;
+
+            List<byte> bytelist = new List<byte>();
+            byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length);
+            while ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
+            {
+                //string data = Encoding.ASCII.GetString(receivedBytes, 0, byte_count);
+                //Console.Write(Encoding.ASCII.GetString(receivedBytes, 0, byte_count));
+            }
+            string data = Encoding.ASCII.GetString(receivedBytes, 0, byte_count);
+            byte[] _buffer = Encoding.ASCII.GetBytes(data);
+            string d_id = BitConverter.ToString(_buffer, 0);
+            Console.WriteLine("printing!!!!" + d_id);
+            //string room = BitConverter.ToString(_buffer, 1, 7);
+            //string status = BitConverter.ToString(_buffer, 8);
+
+            //bytelist.AddRange(Encoding.ASCII.GetBytes(d_id));
+            //bytelist.AddRange(Encoding.ASCII.GetBytes(room));
+            //bytelist.AddRange(Encoding.ASCII.GetBytes(status));
+            //bytelist.ToArray();
+
+            //Console.WriteLine("printing!!!!" + bytelist);
+        }
+    }
+}
---
+++ Server_Tutorial/Server_Tutorial/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace Server_Tutorial
+{
+    class Program
+    {
+
+
+        //static void Main(string[] args)
+        //{
+        //    int port = 13000;
+        //    string IpAddress = "127.0.0.1";
+
+        //    //var myprotocol = new byte[] { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 };
+
+        //    Socket ServerListener = new Socket(AddressFamily
+        //        .InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //    IPEndPoint ep = new IPEndPoint(IPAddress.Parse(IpAddress), port);
+        //    ServerListener.Bind(ep);
+        //    ServerListener.Listen(100);
+        //    Console.WriteLine("Server is Listening...");
+        //    Socket ClientSocket = default(Socket);
+        //    int counter = 0;
+        //    Program p = new Program();
+        //    while (true)
+        //    {
+        //        counter++;
+        //        ClientSocket = ServerListener.Accept();
+        //        Console.WriteLine(counter + "Client connected");
+        //        Thread UserThread = new Thread(new ThreadStart(() =>p.User(ClientSocket)));
+        //        UserThread.Start();
+        //    }
+        //}
+        //public void User(Socket client)
+        //{   while (true)
+        //    {
+        //        byte[] msg = new byte[1024];
+        //        int size = client.Receive(msg);
+        //        client.Send(msg, 0, size, SocketFlags.None);
+        //    }
+        //}
+        public static List<Session> all_session = new List<Session>();
+        public class Session
+        {
+            public string id { get; set; }
+            public TcpClient _socket { get; set; }
+
+            public void receive()
+            {
+                //int id = (int)o;
+                //Console.WriteLine(all_session);
+                while (true)
+                {
+                    NetworkStream stream = _socket.GetStream();
+                    //Console.WriteLine(stream);
+                    byte[] buffer = new byte[1024];
+                    int byte_count = stream.Read(buffer, 0, buffer.Length);
+                    //Console.WriteLine(byte_count);
+
+                    if (byte_count == 0)
+                    {
+                        break;
+                    }
+
+
+                    //verify
+                    if(byte_count >=6)
+                    {
+                        if(buffer[0]==0xff)  //check header
+                        {
+                            int len = buffer[1];
+
+                            byte[] data = new byte[byte_count - 3];
+
+                            Array.Copy(buffer, 3, data, 0, data.Length);
+
+                            string str = Encoding.ASCII.GetString(data, 0, data.Length);
+
+                            Console.WriteLine(str);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("wrong header");
+                        }
+                    }
+                    else{
+                        Console.WriteLine("unknow package");
+                    }
+
+
+                    //test(); 
+                    //string data = "1bedroom1";
+                    ////string data = Encoding.ASCII.GetString(buffer, 0, byte_count); //It's string
+                    ////broadcast(data);
+                    //NetworkStream _stream = _socket.GetStream();
+                    //byte[] _buffer = Encoding.ASCII.GetBytes(data);
+
+                    ////string d_id = BitConverter.ToString(_buffer, 0);
+                    ////string room = BitConverter.ToString(_buffer, 1, 7);
+                    ////string status = BitConverter.ToString(_buffer, 8);
+
+                    //_stream.Write(_buffer, 0, _buffer.Length);
+                    //Console.WriteLine(_buffer.ToString());
+
+                    //byte[] buffer = Encoding.ASCII.GetBytes(data);
+                }
+
+                //lock (_lock) list_clients.Remove(id);
+                _socket.Client.Shutdown(SocketShutdown.Both);
+                _socket.Close();
+            }
+        }
+
+        //static readonly object _lock = new object();
+        static readonly Dictionary<int, TcpClient> list_clients = new Dictionary<int, TcpClient>();
+
+        static void Main(string[] args)
+        {
+            int count = 1;   //จะเอา count มาเป็น id
+
+            TcpListener ServerSocket = new TcpListener(IPAddress.Any, 5000);
+            ServerSocket.Start();
+
+            while (true)
+            {
+                
+                TcpClient client = ServerSocket.AcceptTcpClient();
+               
+                Session new_session = new Session();
+                new_session._socket = client; 
+                new_session.id = count.ToString();
+                all_session.Add(new_session);  
+                //Console.WriteLine(new_session);
+                
+                Thread t = new Thread(new_session.receive);
+
+                t.Start();  
+                count++;
+            }
+        }
+
+        //public static void handle_clients(object o)
+        //{
+        //    int id = (int)o;
+        //    TcpClient client;
+
+        //    lock (_lock) client = list_clients[id];
+
+        //    while (true)
+        //    {
+        //        NetworkStream stream = client.GetStream();
+        //        //Console.WriteLine(stream);
+        //        byte[] buffer = new byte[1024];
+        //        int byte_count = stream.Read(buffer, 0, buffer.Length);
+        //        //Console.WriteLine(byte_count);
+
+        //        if (byte_count == 0)
+        //        {
+        //            break;
+        //        }
+        //        //test(); 
+        //        string data = "101bedroom100";
+        //        //string data = Encoding.ASCII.GetString(buffer, 0, byte_count); //It's string
+        //        broadcast(data);
+               
+        //        //byte[] buffer = Encoding.ASCII.GetBytes(data);
+        //        Console.WriteLine(Encoding.ASCII.GetBytes(data));
+        //    }
+
+        //    lock (_lock) list_clients.Remove(id);
+        //    client.Client.Shutdown(SocketShutdown.Both);
+        //    client.Close();
+        //}
+
+        //public static void test()
+        //{
+        //    string data = "101bedroom100";
+        //    //byte[] buffer = Encoding.ASCII.GetBytes(data);
+        //    Console.WriteLine(Encoding.ASCII.GetBytes(data));
+        //}
+
+        //public static void broadcast(string data)
+        //{
+        //    byte[] buffer = Encoding.ASCII.GetBytes(data + Environment.NewLine);
+
+        //    lock (_lock)
+        //    {
+        //        foreach (TcpClient c in list_clients.Values)
+        //        {
+        //            NetworkStream stream = c.GetStream();
+
+        //            stream.Write(buffer, 0, buffer.Length);
+        //        }
+        //    }
+        //}
+
+    }
+    
+
+   
+}
